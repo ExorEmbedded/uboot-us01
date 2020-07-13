8396df73ff12efae45d19c907d286572c52a16cb
Giovanni Pavoni - Exorint <giovanni.pavoni@exorint.com>
2020-07-13 12:12:29 +0200

    BSP-2012 fastboot: Reduced bootdelay to 10ms

--- common/autoboot.c
+++ common/autoboot.c
@@ -189,7 +189,7 @@ static int passwd_abort(uint64_t etime)
 static int __abortboot(int bootdelay)
 {
 	int abort;
-	uint64_t etime = endtick(bootdelay);
+	uint64_t etime = (get_ticks() + ((uint64_t)(bootdelay) * get_tbclk()) / 100);
 
 #  ifdef CONFIG_AUTOBOOT_PROMPT
 	/*
