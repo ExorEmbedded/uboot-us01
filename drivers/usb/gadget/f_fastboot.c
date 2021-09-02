e29efb33139272538a9964d71cf0a04bd5171e69
Giovanni Pavoni - Exorint <giovanni.pavoni@exorint.com>
2021-09-02 12:27:24 +0200

    BSP-2839 NS05: merged the support for the imx8mn cpu NOTE: This commit fixes also the temperature sensor calibration issues seen on imx8mm (US04, NS04) CPUs.
    NOTE: The following patches were backported from: https://source.codeaurora.org/external/imx/uboot-imx, branch: imx_v2018.03_4.14.98_2.2.0
    - MLK-21889-1, MLK-21889-2, ..., MLK-21889-11
    - MLK-21950-2
    - MLK-22047
    - MLK-22080
    - MLK-22192
    - MLK-20574
    - MLK-22258
    - MLK-20784-01, MLK-20784-02
    - MLK-22425

--- drivers/usb/gadget/f_fastboot.c
+++ drivers/usb/gadget/f_fastboot.c
@@ -759,7 +759,7 @@ U_BOOT_CMD(
 
 static ulong bootloader_mmc_offset(void)
 {
-	if (is_imx8m() || (is_imx8() && is_soc_rev(CHIP_REV_A)))
+	if (is_imx8mq() || is_imx8mm() || (is_imx8() && is_soc_rev(CHIP_REV_A)))
 		return 0x8400;
 	else if (is_imx8qm()) {
 		if (MEK_8QM_EMMC == fastboot_devinfo.dev_id)
@@ -767,6 +767,12 @@ static ulong bootloader_mmc_offset(void)
 			return 0x0;
 		else
 		/* target device is SD card, bootloader offset is 0x8000 */
+			return 0x8000;
+	} else if (is_imx8mn()) {
+		/* target device is eMMC boot0 partition, bootloader offset is 0x0 */
+		if (env_get_ulong("emmc_dev", 10, 1) == fastboot_devinfo.dev_id)
+			return 0;
+		else
 			return 0x8000;
 	}
 	else if (is_imx8())
@@ -1611,6 +1617,9 @@ void board_fastboot_setup(void)
 	} else if (is_imx8mm()) {
 		if (!env_get("soc_type"))
 			env_set("soc_type", "imx8mm");
+	} else if (is_imx8mn()) {
+		if (!env_get("soc_type"))
+			env_set("soc_type", "imx8mn");
 	}
 }
 
